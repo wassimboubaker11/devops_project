43a72c68fea04f2ea6767c79c243595f6ff34913
wassimboubaker11 <93349128+wassimboubaker11@users.noreply.github.com>
2022-11-11 15:18:48 +0100

    Update StockServiceImplTest.java

--- src/test/java/tn/esprit/rh/achat/StockServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/StockServiceImplTest.java
@@ -1,4 +1,4 @@
-package com.esprit.examen.services;
+package tn.esprit.rh.achat;
 
 import static org.junit.Assert.*;
 import java.util.List;
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import com.esprit.examen.entities.Stock;
+import tn.esprit.rh.entities.Stock;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
